2f5783c3ba7bef945f60f662e1401348989a5667
Simon Frost <simonhfrost@gmail.com>
2024-04-13 12:54:48 -0600

    Rotate dumpster

--- Frontend/src/components/screens/Index.tsx
+++ Frontend/src/components/screens/Index.tsx
@@ -10,56 +10,16 @@ function Index() {
 
   return (
     <>
-      <Head title="TOP PAGE" />
+      <Head title="Dorado Dumpster"/>
       <div className="hero min-h-screen">
         <div className="text-center hero-content">
+          <img className="image-rotate" src="src/dumpster-hero.png" width="200px"/>
           <div>
             <h1 className="text-3xl font-bold">
-              <a className="link link-primary" target="_blank" href="https://vitejs.dev/" rel="noreferrer">
-                Vite
-              </a>{' '}
-              +{' '}
-              <a className="link link-primary" target="_blank" href="https://reactjs.org/" rel="noreferrer">
-                React
-              </a>{' '}
-              +{' '}
-              <a className="link link-primary" target="_blank" href="https://www.typescriptlang.org/" rel="noreferrer">
-                TypeScript
-              </a>{' '}
-              +{' '}
-              <a className="link link-primary" target="_blank" href="https://tailwindcss.com/" rel="noreferrer">
-                TailwindCSS
-              </a>{' '}
-              Starter
+              Dorado Dumpster
             </h1>
             <p className="mt-4 text-lg">
-              For fast <b>prototyping</b>. Already set up{' '}
-              <a
-                className="link link-primary"
-                target="_blank"
-                href="https://github.com/firebase/firebase-js-sdk"
-                rel="noreferrer"
-              >
-                Firebase(v9)
-              </a>
-              ,{' '}
-              <a className="link link-primary" target="_blank" href="https://daisyui.com/" rel="noreferrer">
-                daisyUI
-              </a>
-              ,{' '}
-              <a className="link link-primary" target="_blank" href="https://github.com/eslint/eslint" rel="noreferrer">
-                ESLint
-              </a>
-              ,{' '}
-              <a
-                className="link link-primary"
-                target="_blank"
-                href="https://github.com/prettier/prettier"
-                rel="noreferrer"
-              >
-                Prettier
-              </a>
-              .
+              Find your next treasure
             </p>
           </div>
         </div>
