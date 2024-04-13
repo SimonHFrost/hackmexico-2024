13cebe69dc7a8dc9e9335d8d885d8bafadaee501
Simon Frost <simonhfrost@gmail.com>
2024-04-13 14:55:44 -0600

    Extract addIcon method

--- Frontend/src/components/screens/Map.tsx
+++ Frontend/src/components/screens/Map.tsx
@@ -3,6 +3,19 @@ import 'leaflet/dist/leaflet.css';
 import { useEffect } from 'react';
 
 function MapPage() {
+    function addIcon(map: L.Map, latLng: Array<number>) {
+        var icon = L.icon({
+            iconUrl: 'src/map-icon.png',
+            iconSize:     [32, 32], // size of the icon
+            shadowSize:   [50, 64], // size of the shadow
+            iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+            shadowAnchor: [4, 62],  // the same for the shadow
+            popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+        });
+
+        L.marker([latLng[0], latLng[1]], {icon: icon}).addTo(map);
+    }
+
     useEffect(() => {
         const map = L.map('the-map', {
             center: L.latLng(19.4988265,-99.1422943),
@@ -19,17 +32,9 @@ function MapPage() {
             crossOrigin: true
         }).addTo(map);
 
-        var greenIcon = L.icon({
-            iconUrl: 'src/map-icon.png',
-            iconSize:     [32, 32], // size of the icon
-            shadowSize:   [50, 64], // size of the shadow
-            iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-            shadowAnchor: [4, 62],  // the same for the shadow
-            popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-        });
-
-        L.marker([19.4988265,-99.1422943], {icon: greenIcon}).addTo(map);
+        addIcon(map, [19.4988265,-99.1422943]);
     }, []);
+
 
     return (
         <div>
